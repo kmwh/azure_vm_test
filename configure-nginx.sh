14f65f9c605f0781c44507d33172fd000a92603d
kwh <kmwh030@gmail.com>
2025-04-26 03:03:28 +0900

    Initial commit

---
+++ configure-nginx.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Update apt cache.
+sudo apt-get update
+
+#We need to add repo before installing nginx-core. Otherwise we get an error
+sudo add-apt-repository main
+sudo add-apt-repository universe
+sudo add-apt-repository restricted
+sudo add-apt-repository multiverse  
+
+# Install Nginx.
+sudo apt-get install -y nginx
+
+# Set the home page.
+echo "<html><body><h2>202112304 김원호</h2></body></html>" | sudo tee -a /var/www/html/index.html
